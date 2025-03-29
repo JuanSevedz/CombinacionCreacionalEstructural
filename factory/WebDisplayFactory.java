dc04d5d992e6b403f0add7018f18974ddd9bfeb0
Felipe Cardenas Mora <fellippex06@hotmail.com>
2025-03-28 21:38:35 -0500

    Finalizando cambios en ramaPipe

--- factory/WebDisplayFactory.java
+++ factory/WebDisplayFactory.java
@@ -1,6 +1,25 @@
 package factory;
 
+import products.Input;
+import products.Output;
+import products.web.WebInput;
+import products.web.WebOutput;
+
+
 public class WebDisplayFactory {
+
+  @Override
+  public Input createInput() {
+    return new WebInput();
+  }
+
+
+  @Override
+  public Output createOutput() {
+    return new WebOutput();
+  }
+
+
 
 
   
--- products/web/WebInput.java
+++ products/web/WebInput.java
@@ -1,5 +1,17 @@
 package products.web;
 
+import products.Input;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
 public class WebInput {
+
+  @Override
+  public Object getData() {
+    try {
+      
+    }
+  }
   
 }
