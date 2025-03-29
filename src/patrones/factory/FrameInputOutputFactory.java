a4bb5479db7905d69eaddc8887c7640d9845f984
AlejandroRoa28 <alejoroa2006@gmail.com>
2025-03-28 23:42:50 -0500

    Corrección en Client.java: ahora usa sendData() en Output

--- src/patrones/factory/FrameInputOutputFactory.java
+++ src/patrones/factory/FrameInputOutputFactory.java
@@ -1,16 +1,18 @@
 package src.patrones.factory;
 
 import src.patrones.input.FrameInput;
+import src.patrones.input.Input;
 import src.patrones.output.FrameOutput;
+import src.patrones.output.Output;
 
 public class FrameInputOutputFactory implements InputOutputFactory {
-  @Override
-  public FrameInput createInput() {
-    return new FrameInput();
-  }
+    @Override
+    public Input createInput() {
+        return new FrameInput();
+    }
 
-  @Override
-  public FrameOutput createOutput() {
-    return new FrameOutput();
-  }
+    @Override
+    public Output createOutput() {
+        return new FrameOutput();
+    }
 }
--- src/patrones/input/FrameInput.java
+++ src/patrones/input/FrameInput.java
@@ -1,5 +1,10 @@
 package src.patrones.input;
 
+import javax.swing.JOptionPane;
+
 public class FrameInput implements Input {
-  
+    @Override
+    public Object getData() {
+        return JOptionPane.showInputDialog(null, "Ingresa los datos:");
+    }
 }
